287c440a94c9ccf8b69c5b2ea261633127efd84e
hannafuerst <fuerst13@gmail.com>
2020-03-08 10:57:16 +0100

    generyka

---
+++ src/main/java/Generyka.java
@@ -0,0 +1,11 @@
+public class Generyka <T>{
+    private T field;
+
+    public T getField() {
+        return field;
+    }
+
+    public Generyka(T field) {
+        this.field = field;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Generyka <String> generyka= new Generyka<String> ("Test");
+        System.out.println(generyka.getField());
+        Generyka <Integer> generyka1= new Generyka <Integer>;
+        System.out.println(generyka1.getField());
+    }
+
+
+    }
